7ac629bdd3394d5e8a760d8f420c464334007a4a
zmzehd <mahammadmmdvdev@gmail.com>
2025-07-22 19:52:02 +0400

    python script to convert markdown files to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+        sys.exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
     if not os.path.isfile(input_file):
         rint(f"Missing {input_file}", file=sys.stderr)
-    sys.exit(1)
+        sys.exit(1)
 
     sys.exit(0)
