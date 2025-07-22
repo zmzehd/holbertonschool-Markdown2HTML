4a7c759907dd205d86495501ebd4f8c9b6d78fef
zmzehd <mahammadmmdvdev@gmail.com>
2025-07-22 19:54:26 +0400

    diff --git a/markdown2html.py b/markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     output_file = sys.argv[2]
 
     if not os.path.isfile(input_file):
-        rint(f"Missing {input_file}", file=sys.stderr)
+        print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
 
     sys.exit(0)
