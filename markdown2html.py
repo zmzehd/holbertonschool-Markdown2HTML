25a2837db38955156f8a2cb0cf547aafe6bada63
zmzehd <mahammadmmdvdev@gmail.com>
2025-07-22 20:04:32 +0400

    lasgt one for pep !!!!!!!!!!!!!

--- markdown2html.py
+++ markdown2html.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
     # your existing code here
 
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", 
-        file=sys.stderr)
+        print(
+            "Usage: ./markdown2html.py README.md README.html",
+            file=sys.stderr
+        )
         sys.exit(1)
 
     input_file = sys.argv[1]
